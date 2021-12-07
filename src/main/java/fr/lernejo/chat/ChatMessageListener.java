b91136bfaabf31e56216b3f8853f29910a97e3e7
srikanth collaty <srikanthcollaty92@gmail.com>
2021-12-07 18:12:20 +0100

    modif class listener

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -3,12 +3,8 @@ package fr.lernejo.chat;
 import org.springframework.stereotype.Component;
 
 @Component
-public class  ChatMessageListener {
-private String message;
-    public String onMessage(String message){
-        this.message=message;
-          System.out.println(message);
-          return message;
-
+public class ChatMessageListener {
+    public void onMessage(String message) {
+        System.out.println(message);
     }
 }
